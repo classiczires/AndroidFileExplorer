dbb6452d21cd35bb9f8da21f548980c0ff705306
saeed karimi <classiczires@gmail.com>
2019-10-25 16:51:21 +0330

    create main activity

---
+++ app/src/main/java/com/zires/androidfileexplorer/main/MainActivity.java
@@ -0,0 +1,16 @@
+package com.zires.androidfileexplorer.main;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.zires.androidfileexplorer.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
