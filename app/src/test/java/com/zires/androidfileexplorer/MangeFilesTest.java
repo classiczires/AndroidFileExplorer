7291b3e90f70a7612ecb524c9a442bd8cc19e111
saeed karimi <classiczires@gmail.com>
2019-11-03 22:52:32 +0330

    create tests

--- app/src/test/java/com/zires/androidfileexplorer/MangeFilesTest.java
+++ app/src/test/java/com/zires/androidfileexplorer/MangeFilesTest.java
@@ -1,25 +1,22 @@
 package com.zires.androidfileexplorer;
 
-import com.zires.androidfileexplorer.model.File;
 import com.zires.androidfileexplorer.model.Folder;
 import com.zires.androidfileexplorer.model.User;
 
 import org.junit.Before;
-import org.junit.FixMethodOrder;
 import org.junit.Test;
-import org.junit.runners.MethodSorters;
 
 import java.util.Date;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
 /**
  * Created by ClassicZires on 10/29/2019.
  **/
-@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+
 public class MangeFilesTest {
     private Folder folder;
+    private String folderName = "ZiresFolder";
     private String fileName = "file";
 
     @Before
@@ -29,19 +26,45 @@ public class MangeFilesTest {
     }
 
     @Test
-    public void firstCreateFileThenRenameTheFileAndFinallyDeleteTheFile() {
-        //create
+    public void createRenameDeleteFile() {
         folder.createFile(fileName);
         assertEquals(folder.getFiles().get(0).getName(),
                 fileName.concat(".").concat(folder.getFiles().get(0).getFormat()));
 
-        //rename
         fileName = "newFileName";
         folder.rename(fileName, 0);
         assertEquals(folder.getFiles().get(0).getName(), fileName);
 
-        //delete
         folder.delete(fileName);
         assertEquals(folder.getFiles().size(), 0);
+    }
+
+    @Test
+    public void createRenameDeleteFolder() {
+        //folder
+        folder.createFolder(folderName);
+        assertEquals(folder.getFolders().get(0).getName(), folderName);
+
+        folderName = "newFolderName";
+        folder.rename(folderName, 0);
+        assertEquals(folder.getFolders().get(0).getName(), folderName);
+
+        folder.delete(folderName);
+        assertEquals(folder.getFolders().size(), 0);
+    }
+
+    @Test
+    public void calculateFolderSize() {
+        long folderSize = 0;
+        folder.createFolder("folder1");
+
+        folder.getFolders().get(0).createFile("file0");
+        folderSize += folder.getFolders().get(0).getFiles().get(0).getSize();
+        folder.createFile("file1");
+        folderSize += folder.getFiles().get(0).getSize();
+        folder.createFile("file2");
+        folderSize += folder.getFiles().get(1).getSize();
+
+        assertEquals(folder.getSize(), folderSize);
     }
 }
